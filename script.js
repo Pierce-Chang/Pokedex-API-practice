82ec0b9cc84907b743acad3a406bb976799893e0
Pierce Chang <pierce@mailbox.org>
2023-09-28 20:41:45 +0200

    Addet Name and Image from API into HTML using

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ async function loadPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
     let response = await fetch(url);
     currentPokemon = await response.json();
-    console.log('Loaded Pokemon', responseAsJson)
+    console.log('Loaded Pokemon', currentPokemon)
 
     renderPokemonInfo();
 }
 
 function renderPokemonInfo() {
-    
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+    document.getElementById('pokemonImage').src = currentPokemon['sprites']['front_default'];
 }
