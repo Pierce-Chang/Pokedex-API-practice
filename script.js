d4fc06734e54a6fb6e60e3275ee6c965dd99085d
Pierce Chang <pierce@mailbox.org>
2023-09-30 20:53:27 +0200

    addet bootstrap, navbar and Style

--- script.js
+++ script.js
@@ -10,12 +10,19 @@ async function loadPokemon() {
 }
 
 function renderPokemonInfo() {
-    let correctedName = capitalizeFirstLetter(currentPokemon['name']);
-    document.getElementById('pokemonName').innerHTML = correctedName;
-    document.getElementById('pokemonType').innerHTML = currentPokemon['types']['0']['type']['name'];
+    let formattedName = capitalizeFirstLetter(currentPokemon['name']);
+    let formattedType = capitalizeFirstLetter(currentPokemon['types']['0']['type']['name']);
+    let formattedId = formatPokemonId(currentPokemon['id']);
+    document.getElementById('pokemonName').innerHTML = formattedName;
+    document.getElementById('pokemonType').innerHTML = formattedType;
+    document.getElementById('pokemonId').innerHTML = formattedId;
     document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
 }
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function formatPokemonId(id) {
+    return '#' + ('000' + currentPokemon['id']).slice(-3);
 }
